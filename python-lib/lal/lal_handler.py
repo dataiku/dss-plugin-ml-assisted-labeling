a51ab89241fc8f960fc334033a224b4abbbaaf11
admin <admin>
2019-10-22 11:43:10 +0200

    Edited files

--- python-lib/lal/lal_handler.py
+++ python-lib/lal/lal_handler.py
@@ -26,8 +26,8 @@ class LALHandler(object):
             queries_df = dataiku.Dataset(self.config["queries_ds"]).get_dataframe()
             queries_df = queries_df.sort_values('uncertainty', ascending=False)['id']
             print(queries_df.head())
-            remaining = queries_df[~queries_df['id'].isin(self.classifier.get_labeled_sample_ids())]['id'].unique().tolist()
             print('remaining')
+            remaining = queries_df[~queries_df['id'].isin(self.classifier.get_labeled_sample_ids())]['id'].unique().tolist()
             return remaining
         except:
             self.logger.info("Not taking into account uncertainty, serving random queries")
