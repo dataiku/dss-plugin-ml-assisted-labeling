f450ec384bd39f1659e280760e4a4692a543d90e
Andrey Avtomonov <andreybavt@gmail.com>
2019-11-18 17:22:09 +0100

    sound labeling

---
+++ webapps/sound-labeling/backend.py
@@ -0,0 +1,16 @@
+import dataiku
+from dataiku.customwebapp import get_webapp_config
+from lal.api import define_endpoints
+from lal.app_configuration import prepare_datasets
+from lal.classifiers.sound_classifier import SoundClassifier
+
+config = get_webapp_config()
+
+labels_schema = [
+    {"name": "path", "type": "string"}
+]
+prepare_datasets(labels_schema)
+
+unlabeled_mf = dataiku.Folder(config["unlabeled"])
+queries_df = dataiku.Dataset(config["queries_ds"]).get_dataframe()
+define_endpoints(app, SoundClassifier(unlabeled_mf, queries_df, config))
