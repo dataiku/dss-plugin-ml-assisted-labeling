7a03bc1543453dbd8920fe1e043c0f207e3e36fb
Andrey Avtomonov <andreybavt@gmail.com>
2019-12-13 15:42:05 +0100

    increase session for queries each time sampler runs

--- custom-recipes/active-learning-query-sampler/recipe.py
+++ custom-recipes/active-learning-query-sampler/recipe.py
@@ -18,6 +18,7 @@ except Exception as e:
     unlabeled_df = pd.DataFrame(unlabeled_samples.list_paths_in_partition(), columns=["path"])
 
 model = dataiku.Model(get_input_names_for_role('saved_model')[0])
+queries_ds = dataiku.Dataset(get_output_names_for_role('queries')[0], ignore_flow=True)
 
 clf = model.get_predictor()._clf
 X = model.get_predictor().get_preprocessing().preprocess(unlabeled_df)[0]
@@ -28,10 +29,13 @@ strategy_mapper = {
     'entropy': uncertainty.entropy_sampling
 }
 
+queries_df = queries_ds.get_dataframe()
+current_session = 0 if queries_df.session.empty else queries_df.session.max() + 1
+
 func = strategy_mapper[config['strategy']]
 index, uncertainty = func(clf, X=X, n_instances=unlabeled_df.shape[0])
 unlabeled_df = unlabeled_df.loc[index]
 unlabeled_df['uncertainty'] = uncertainty
+unlabeled_df['session'] = current_session
 
-queries = dataiku.Dataset(get_output_names_for_role('queries')[0])
-queries.write_with_schema(unlabeled_df)
+queries_ds.write_with_schema(unlabeled_df)
