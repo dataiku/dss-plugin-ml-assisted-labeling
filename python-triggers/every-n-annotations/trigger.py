4100131e2b2c597210b1f0e0c863db9a8a7ac622
admin <admin>
2019-12-16 15:15:29 +0100

    Edited file 'python-triggers/every-n-annotations/trigger.py'

--- python-triggers/every-n-annotations/trigger.py
+++ python-triggers/every-n-annotations/trigger.py
@@ -1,43 +1,23 @@
-# This file is the actual code for the custom Python trigger every-n-annotations
-
-import os, json
 import dataiku
-
 from dataiku.customtrigger import *
 from dataiku.scenario import Trigger
 
-# the settings of the user for this instance of the trigger
+
 trigger_config = get_trigger_config()
 
 t = Trigger()
 
-annotations_count = trigger_config['annotations_count']
-annotations = dataiku.Dataset(trigger_config['dataset'])
-annotations_df = annotations.get_dataframe()
-#stop_annotations = trigger_config['stop_annotations']
-#stop_annotations_count = trigger_config['stop_annotations_count']
-#stop_model = trigger_config['stop_model']
-#stop_model_df = dataiku.Dataset(trigger_config['stop_model_dataset']).get_dataset()
-#stop_model_column = trigger_config['stop_model_column']
-#stop_model_epsilon = trigger_config['stop_model_epsilon']
-last_session_run = trigger_config['stop_model_epsilon']
+labeling_count = int(trigger_config['i_labeling_count'])
+metadata_df = dataiku.Dataset(trigger_config['ds_metadata']).get_dataframe()
+queries_df = dataiku.Dataset(trigger_config['ds_queries']).get_dataframe()
 
 trigger = False
 
-# Get session computed from annotations dataset
-max_session = annotations_df['session'].max()
-n_annotations = (annotations_df['session'] == max_session).sum()
-trigger = (max_session > last_session_run and n_annotations >= annotations_count)
-if trigger:
-    trigger_config['last_session_run'] = max_session
-
-#if stop_annotations:
-#    trigger = (trigger and (annotations_df.shape[0] < stop_annotations_count)) 
-#
-#if stop_model and stop_model_df.shape[0] >= 2:
-#    stop_model_df = stop_model_df.sort_values('date')
-#    metrics = stop_model_df[stop_model_column]
-#    trigger = (trigger and (np.abz(metrics[-1] - metrics[-2]) > stop_model_epsilon))
+# Metadata dataset contains labels corresponding to the last sessions labeled.
+# Queries dataset contains queries corresponding to the next sessions to label.
+next_session = queries_df['session'].max()
+n_labeling = (metadata_df['session'] == next_session).sum()
+trigger = (n_labeling >= labeling_count)
 
 if trigger:
     t.fire()
