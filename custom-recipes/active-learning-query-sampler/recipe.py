3ce377554f9a0a1ec28a1fc4f71ad4aac915a7ff
admin <admin>
2019-12-20 08:36:42 +0100

    Edited files

--- custom-recipes/active-learning-query-sampler/recipe.py
+++ custom-recipes/active-learning-query-sampler/recipe.py
@@ -11,9 +11,6 @@ from dss_utils import get_input_full_name
 
 # Load configuration
 config = get_recipe_config()
-print('§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§')
-print(get_input_names_for_role('unlabeled_samples'))
-print('§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§')
 unlabeled_samples_container = get_input_names_for_role('unlabeled_samples')[0]  # get_input_full_name('unlabeled_samples')
 saved_model_id = get_input_names_for_role('saved_model')[0]
 model = dataiku.Model(saved_model_id)
