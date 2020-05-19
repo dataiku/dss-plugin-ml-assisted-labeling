6104e1de2d24031465a7b01c6f806bdfe9339a6e
Alexandre Abraham <alexandre.abraham@dataiku.com>
2020-05-19 09:50:29 +0200

    Always set the first 20 samples as green

--- custom-recipes/active-learning-query-sampler/recipe.py
+++ custom-recipes/active-learning-query-sampler/recipe.py
@@ -2,6 +2,7 @@ import logging
 import pandas as pd
 from pickle import PickleError
 from json import JSONDecodeError  
+import numpy as np
 
 import dataiku
 from dataiku.customrecipe import *
@@ -13,6 +14,8 @@ from lal import utils
 # Load configuration
 config = get_recipe_config()
 unlabeled_samples_container = get_input_names_for_role('unlabeled_samples')[0]
+unlabeled_test_samples_container = get_input_names_for_role('unlabeled_test')
+unlabeled_test_samples_container = unlabeled_test_samples_container[0] if len(unlabeled_test_samples_container) > 0 else None
 saved_model_id = get_input_names_for_role('saved_model')[0]
 model = dataiku.Model(saved_model_id)
 queries_ds = dataiku.Dataset(get_output_names_for_role('queries')[0], ignore_flow=True)
@@ -23,60 +26,19 @@ strategy_mapper = {
     'entropy': uncertainty.entropy_sampling
 }
 
-# Helper functions
-def prettify_error(s):
-    """Adds a blank and replaces regular spaces by non-breaking in the first 90 characters
-    
-    This function adds a big blank space and forces the first words to be a big block of
-    unbreakable words. This enforces a newline in the DSS display and makes the error prettier.
-    """
-    return '\xa0' * 130 + ' \n' + s[:90].replace(' ', '\xa0') + s[90:]
+clf = utils.load_classifier(model)
 
-# DSS entities loading
-logging.info("Reading unlabeled samples from {0}".format(unlabeled_samples_container))
-unlabeled_is_folder = False
-try:
-    unlabeled_df = pd.DataFrame(dataiku.Dataset(unlabeled_samples_container).get_dataframe())
-    logging.info("Unlabeled input is a dataset")
-except Exception as e:
-    logging.info("Unlabeled input is a folder: {0}".format(e))
-    unlabeled_samples = dataiku.Folder(unlabeled_samples_container)
-    unlabeled_df = pd.DataFrame(unlabeled_samples.list_paths_in_partition(), columns=["path"])
-    unlabeled_is_folder = True
+#################
+# Active learning
 
-logging.info("Trying to load model from {0}".format(saved_model_id))
-try:
-    clf = model.get_predictor()._clf
-except Exception as e:
-    raise PickleError(
-        utils.prettify_error('Failed to load the saved model. The visual Machine Learning '
-                             'code environment needs to be compatible with the code environment of this plugin.' ) +
-        utils.prettify_error('Original error is {}'.format(e)))
-
-logging.info("Checking that model {0} is a classifier".format(saved_model_id))
-if len(model.get_predictor().classes) == 0:
-    raise TypeError(
-        utils.prettify_error('Saved model {} seems to be a regressor and not a classifier.'.format(saved_model_id) +
-                             'Active learning in regression context is not supported yet.'))
-    
+unlabeled_df, unlabeled_is_folder = utils.load_data(unlabeled_samples_container)
+X = utils.preprocess_data(model, unlabeled_df, unlabeled_is_folder)
+func = strategy_mapper[config['strategy']]
+index, uncertainty_scores = func(clf, X=X, n_instances=X.shape[0])
 utils.increment_queries_session(queries_ds.short_name)
 
-# Active learning
-func = strategy_mapper[config['strategy']]
-try:
-    X = model.get_predictor().get_preprocessing().preprocess(unlabeled_df)[0]
-except Exception as e:
-    if unlabeled_is_folder and ("Failed to preprocess the following file" in str(e) or ('Managed folder name not found' in str(e))):
-        raise LookupError(
-            utils.prettify_error('The model feature preprocessing could not be applied to the folder {}'.format(unlabeled_samples_container) +
-                                 'This happens when the folder specified as image source in the visual '
-                                 'Machine Learning is different from the input folder of this recipe.') +
-            utils.prettify_error('Original error is {}'.format(e)))
-    raise
-    
-index, uncertainty = func(clf, X=X, n_instances=unlabeled_df.shape[0])
 
 # Outputs
 queries_df = unlabeled_df.loc[index]
-queries_df['uncertainty'] = uncertainty
+queries_df['uncertainty'] = uncertainty_scores
 queries_ds.write_with_schema(queries_df)
