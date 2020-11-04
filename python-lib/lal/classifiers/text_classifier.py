e7b93dc9b5db6546bc4e0457c2d390d7886a4fac
api:ElNqO6poD7HIVR6d <api:ElNqO6poD7HIVR6d>
2020-11-04 11:28:44 +0100

    Edited file 'python-lib/lal/classifiers/text_classifier.py'

--- python-lib/lal/classifiers/text_classifier.py
+++ python-lib/lal/classifiers/text_classifier.py
@@ -13,7 +13,7 @@ class TextClassifier(TableBasedDataClassifier):
         super(TextClassifier, self).__init__(queries_df, config)
 
     def get_initial_df(self):
-        return self.__initial_df[self.text_column]
+        return pd.DataFrame(self.__initial_df[self.text_column])
 
     def serialize_label(self, label):
         return json.dumps(label)
