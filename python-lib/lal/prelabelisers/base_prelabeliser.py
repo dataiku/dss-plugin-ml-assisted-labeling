47384236fea0d64e0d93f6f9905b3e4f759894f2
HENRI CHABERT <chabhenrib@gmail.com>
2020-11-18 17:49:59 +0100

    Deleted unused files

--- python-lib/lal/prelabelisers/base_prelabeliser.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-class BasePrelabeliser:
-    def __init__(self, meta_df):
-        self.meta_df = meta_df
--- python-lib/lal/prelabelisers/text_prelabeliser.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from lal.prelabelisers.base_prelabeliser import BasePrelabeliser
-
-class TextPrelabeliser(BasePrelabeliser):
-    def __init__(self):
-        super(TextPrelabeliser, self).__init__()
-
