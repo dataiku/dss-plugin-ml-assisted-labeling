15c53c58442a596278ab340f38d844b43271efec
HENRI CHABERT <chabhenrib@gmail.com>
2020-11-20 00:17:03 +0100

    Changed buttons and empty right panel

--- resource/labeling-app/components/category-selector.js
+++ resource/labeling-app/components/category-selector.js
@@ -200,10 +200,19 @@ let CategorySelector = {
                         <i v-if="status === 'SKIPPED'" class="fas fa-forward skipped"></i>
                         <h2 v-if="status === 'SKIPPED'">Sample was skipped</h2>
                     </div>
-                    <div v-else>
-                        <div v-if="status !== 'SKIPPED'" class="circle"></div>
+                    <div v-else style="width: 100%;">
                         <h2 v-if="status !== 'SKIPPED'">No labels yet</h2>
-                        <p>Select a label and draw a box around the target.</p>
+                        <p style="margin: auto;">Select a label and draw a box around the target.</p>
+                        <div class="circles-container">
+                            <div style="max-width: 110px;">
+                                <div class="circle cat-example"></div>
+                                <span>Clicking on the categories buttons</span>
+                            </div>
+                            <div style="max-width: 110px;">
+                                <div class="circle shortcut-example"></div>
+                                <span>Using keyboard shortcuts</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div v-if="annotation?.label?.filter(e=>!e.draft).length"
