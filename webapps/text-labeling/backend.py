3344cae73198728956448d6b10f54deb1ac7c1f5
HENRI CHABERT <chabhenrib@gmail.com>
2020-12-01 16:36:04 +0100

    Removed AL variable

--- webapps/text-labeling/backend.py
+++ webapps/text-labeling/backend.py
@@ -9,6 +9,6 @@ from lal.handlers.dataiku_lal_handler import DataikuLALHandler
 config = get_webapp_config()
 prepare_datasets()
 initial_df = dataiku.Dataset(config["unlabeled"]).get_dataframe()
-queries_df = dataiku.Dataset(config["queries_ds"]).get_dataframe() if "queries_ds" in config else None
+queries_df = None
 
 define_endpoints(app, DataikuLALHandler(TextClassifier(initial_df, queries_df, config), config))
