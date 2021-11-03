83940740f8c01a53912ddabdc05fe1d76ead0c9e
Claire <claire.behue@dataiku.com>
2021-11-03 18:18:06 +0100

    remove unused main notion

--- custom-recipes/ml-assisted-labeling-deephub-export/recipe.py
+++ custom-recipes/ml-assisted-labeling-deephub-export/recipe.py
@@ -23,19 +23,18 @@ def format_labeling_plugin_annotations(image_annotations):
     return image_annotations
 
 
-if __name__ == '__main__':
-    input_dataset = dataiku.Dataset(get_input_names_for_role("input_dataset")[0])
+input_dataset = dataiku.Dataset(get_input_names_for_role("input_dataset")[0])
 
-    # copy all the columns from input dataset (only target one will be changed)
-    output_df = input_dataset.get_dataframe()
+# copy all the columns from input dataset (only target one will be changed)
+output_df = input_dataset.get_dataframe()
 
-    target_column = get_recipe_config()["target_column"]
-    if target_column not in output_df.columns:
-        raise Exception("Column {} not in input_df with columns [{}]".format(target_column, list(output_df.columns)))
+target_column = get_recipe_config()["target_column"]
+if target_column not in output_df.columns:
+    raise Exception("Column {} not in input_df with columns [{}]".format(target_column, list(output_df.columns)))
 
-    logging.info("Annotations target column chosen: {}".format(target_column))
+logging.info("Annotations target column chosen: {}".format(target_column))
 
-    output_df[target_column] = output_df[target_column].map(format_labeling_plugin_annotations)
+output_df[target_column] = output_df[target_column].map(format_labeling_plugin_annotations)
 
-    output_dataset = dataiku.Dataset(get_output_names_for_role("output_dataset")[0])
-    output_dataset.write_with_schema(output_df)
+output_dataset = dataiku.Dataset(get_output_names_for_role("output_dataset")[0])
+output_dataset.write_with_schema(output_df)
